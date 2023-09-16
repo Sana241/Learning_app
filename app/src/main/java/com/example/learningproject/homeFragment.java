59256b0e3af319f3b1fb4f8139df483dc1874599
Sana Yousaf <sanayousaf0103@gmail.com>
2023-09-16 15:18:15 -0700

    Adding Recycler View to home fragment

--- app/src/main/java/com/example/learningproject/homeFragment.java
+++ app/src/main/java/com/example/learningproject/homeFragment.java
@@ -4,30 +4,45 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.util.LinkedList;
+
 public class homeFragment extends Fragment {
 
-Button fragmentButton;
+    public final LinkedList<String> linkedList = new LinkedList<>();
+    Button fragmentButton;
+    RecyclerView recyclerView;
+
     public homeFragment() {
-        // Required empty public constructor
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_home, container, false);
-        fragmentButton=view.findViewById(R.id.home_fragment_btn);
+        View view = inflater.inflate(R.layout.fragment_home,
+                container, false);
+        for (int i = 1; i <= 20; i++) {
+            linkedList.add("Recycle item " + i);
+        }
+        fragmentButton = view.findViewById(R.id.home_fragment_btn);
+        recyclerView = view.findViewById(R.id.recyclerview);
+        recyclerAdapter recyclerAdapter=new recyclerAdapter(getActivity(),linkedList);
+        recyclerView.setAdapter(recyclerAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         fragmentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getActivity(),secondActivity.class));
+                startActivity(new Intent(getActivity(), secondActivity.class));
             }
         });
-    return view;
+
+        return view;
     }
 }
